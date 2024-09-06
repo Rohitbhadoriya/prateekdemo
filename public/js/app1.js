a31966d6dae44127f8d23daffe2d211909099093
Rohitbhadoriya <rohitbhadauriya575@gmail.com>
2024-09-06 16:59:06 +0530

    commit

--- public/js/app1.js
+++ public/js/app1.js
@@ -6,3 +6,9 @@ function printBill() {
 document.getElementById('body').innerHTML = bill
     window.print();
 }
+// Auto Time and Date
+window.onload = function() {
+    let now = new Date();
+    let dateTimeString = now.toLocaleDateString() + " " + now.toLocaleTimeString();
+    document.getElementById('date-time').innerHTML = dateTimeString;
+};
