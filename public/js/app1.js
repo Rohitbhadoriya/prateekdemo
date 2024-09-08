6a38c27b46fc769b1d171f2aa8307ec128838a8c
Rohitbhadoriya <rohitbhadauriya575@gmail.com>
2024-09-08 12:05:20 +0530

    commit

--- public/js/app1.js
+++ public/js/app1.js
@@ -7,22 +7,8 @@ document.getElementById('body').innerHTML = bill
     window.print();
 }
 // Auto Time and Date
-// window.onload = function() {
-//     let now = new Date();
-//     let dateTimeString = now.toLocaleDateString() + " " + now.toLocaleTimeString();
-//     document.getElementById('date-time').innerHTML = dateTimeString;
-// };
 window.onload = function() {
-    // Check if the date and time are already saved in localStorage
-    let savedDateTime = localStorage.getItem('savedDateTime');
-    
-    // If not, set the current date and time and save it
-    if (!savedDateTime) {
-        let now = new Date();
-        savedDateTime = now.toLocaleDateString() + " " + now.toLocaleTimeString();
-        localStorage.setItem('savedDateTime', savedDateTime);
-    }
-
-    // Display the saved date and time
-    document.getElementById('date-time').innerHTML = savedDateTime;
+    let now = new Date();
+    let dateTimeString = now.toLocaleDateString() + " " + now.toLocaleTimeString();
+    document.getElementById('date-time').innerHTML = dateTimeString;
 };
